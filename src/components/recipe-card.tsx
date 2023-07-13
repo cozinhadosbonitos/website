d546a27e9b478710beb7e9b106c4afc2340c6e6f
Bruno Alla <alla.brunoo@gmail.com>
2023-07-13 13:19:04 +0100

    Refactor RecipeCard

--- src/components/recipe-card.tsx
+++ src/components/recipe-card.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
 import { GatsbyImage, IGatsbyImageData } from 'gatsby-plugin-image'
-import { Box, Flex, Heading, Text } from '@chakra-ui/react'
+import { Card, CardBody, Heading, Text } from '@chakra-ui/react'
 
 export interface Recipe {
   id: string
@@ -23,30 +23,28 @@ type RecipeCardProps = {
 export const RecipeCard = ({ recipe }: RecipeCardProps) => {
   return (
     <Link to={recipe.url} title={recipe.title}>
-      <Flex
-        direction={{ base: 'column', md: 'row' }}
-        alignItems="center"
-        margin={6}
-        border="black 3px solid"
+      <Card
+        flexDirection={{ base: 'column', md: 'row' }}
+        overflow="hidden"
+        marginY={6}
+        borderRadius="none"
+        borderWidth={2}
         _hover={{
-          cursor: 'pointer',
           boxShadow:
             '0 8px 15px rgba(0, 0, 0, 0.25), 0 5px 8px rgba(0, 0, 0, 0.06)',
         }}
       >
-        <Box flexShrink={0}>
-          <GatsbyImage image={recipe.photo.asset.gatsbyImageData} alt="" />
-        </Box>
-        <Box padding={4}>
-          <Heading as="h2" size="h4">
+        <GatsbyImage image={recipe.photo.asset.gatsbyImageData} alt="" />
+        <CardBody>
+          <Heading size="lg">
             {recipe.title} <br />
-            <Box as="small" fontSize="0.8em">
+            <Text as="small" fontSize="0.8em">
               {recipe.date}
-            </Box>
+            </Text>
           </Heading>
-          <Text>TODO</Text>
-        </Box>
-      </Flex>
+          <Text py={2}>TODO</Text>
+        </CardBody>
+      </Card>
     </Link>
   )
 }
