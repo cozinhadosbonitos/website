9b91cfedb947740baf1b91db3d4840dbf6a4c55a
Bruno Alla <alla.brunoo@gmail.com>
2023-07-13 11:53:22 +0100

    Create theme

---
+++ src/utils/theme.ts
@@ -0,0 +1,58 @@
+import { extendTheme } from '@chakra-ui/react'
+const customTheme = {
+  fonts: {
+    body: 'Lato, "Times New Roman", Times, serif',
+    heading: 'Karla, Roboto, Arial, sans-serif',
+  },
+  colors: {
+    primary: '#f4a460',
+    primaryDark: '#b66e41',
+    secondary: '#e5e5e5',
+  },
+  styles: {
+    global: {
+      html: {
+        fontSize: '1.25rem',
+        overflowY: 'scroll',
+      },
+      a: {
+        textDecoration: 'none',
+        color: 'primary',
+        '&:active, &:hover': {
+          textDecoration: 'underlined',
+          color: 'primaryDark',
+        },
+      },
+      ':where(svg:not([fill]))': {
+        fill: 'currentColor',
+      },
+    },
+  },
+  components: {
+    Heading: {
+      baseStyle: {
+        marginTop: 6,
+        marginBottom: 2,
+        lineHeight: '1.1',
+        fontWeight: '800',
+        textRendering: 'optimizeLegibility',
+      },
+    },
+    Text: {
+      baseStyle: {
+        marginBottom: 2,
+      },
+    },
+    Link: {
+      baseStyle: {
+        color: 'primary',
+        '&:active, &:hover': {
+          textDecoration: 'none',
+          color: 'primaryDark',
+        },
+      },
+    },
+  },
+}
+
+export const theme = extendTheme(customTheme)
