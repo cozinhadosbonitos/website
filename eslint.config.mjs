a36ca9fc75e307e2323b1c1589992064fd3f07ac
Bruno Alla <alla.brunoo@gmail.com>
2024-11-26 23:14:16 +0100

    Migrate to Eslint 9 flat config

---
+++ eslint.config.mjs
@@ -0,0 +1,56 @@
+import react from 'eslint-plugin-react'
+import prettier from 'eslint-plugin-prettier'
+import globals from 'globals'
+import tsParser from '@typescript-eslint/parser'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
+import { includeIgnoreFile } from '@eslint/compat'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+})
+const gitignorePath = path.resolve(__dirname, '.gitignore')
+
+export default [
+  includeIgnoreFile(gitignorePath),
+  ...compat.extends('prettier'),
+  {
+    plugins: {
+      react,
+      prettier,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+
+      parser: tsParser,
+      ecmaVersion: 9,
+      sourceType: 'module',
+
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          semi: false,
+          singleQuote: true,
+          trailingComma: 'es5',
+        },
+      ],
+    },
+  },
+]
